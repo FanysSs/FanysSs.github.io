cafd64a85739ee2754b8c5ec031efc5e78e32f0b
FanysSs <estefania.rguezb21@gmail.com>
2024-09-24 23:47:18 -0600

    add education branch #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, SkillComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, SkillComponent, EducationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
