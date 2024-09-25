9ce1c169cbbe0091b55d4e51f1b9ef7a5f19f8fd
FanysSs <159165821+FanysSs@users.noreply.github.com>
2024-09-25 01:34:24 -0600

    add interests branch #6 (#18)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { SkillComponent } from './skill/skill.component';
 import { EducationComponent } from './education/education.component';
 import { AchievementsComponent } from './achievements/achievements.component';
 import { LanguagesComponent } from './languages/languages.component';
+import { InterestsComponent } from './interests/interests.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, SkillComponent, EducationComponent, AchievementsComponent, LanguagesComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, SkillComponent, EducationComponent, AchievementsComponent, LanguagesComponent, InterestsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
