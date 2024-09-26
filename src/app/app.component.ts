a8141455ad780e5b9aa57f1f73d6301045358c38
FanysSs <159165821+FanysSs@users.noreply.github.com>
2024-09-25 22:28:53 -0600

    Develop (#19)
    * create header branch #1
    
    * add work_experience branch #2
    
    * add karma.conf
    
    * Create develop_ci_cd.yml
    
    * Update develop_ci_cd.yml (#11)
    
    * update header (#12)
    
    * update work_experience (#13)
    
    * add skills branch #4 (#14)
    
    * add education branch #3 (#15)
    
    * add achievements branch #7 (#16)
    
    * add languages branch #5 (#17)
    
    * add interests branch #6 (#18)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { InterestsComponent } from './interests/interests.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, SkillComponent, EducationComponent, AchievementsComponent, LanguagesComponent, InterestsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
